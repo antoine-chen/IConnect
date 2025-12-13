e5f8580983ef34e02d9c3ad931492eba74308e17
Jc <jeanchristophe.lay0@gmail.com>
2025-12-13 08:58:14 +0100

    Modification mineure sur la structure lié à l'affichage de la page une fois qu'on a choisi une association

--- modules/mod_asso/mod_asso.php
+++ modules/mod_asso/mod_asso.php
@@ -7,26 +7,23 @@ class ModAsso {
     {
         // Initialise le controleur
 
-        if(!isset($_SESSION['asso'])) { // Si l'utilisateur n'a pas choisi d'associations
-            // Appelle le controleur de asso pour afficher la liste des assos
-        }
-        else { // Si l'utilisateur a pas choisi un asso
-            $module = $_GET['module'];
-            switch ($module) {
-                case 'panier':
-                    // new ModPanier
-                    break;
-                case 'boutique':
-                    // New mod boutique
-                    break;
-                case 'stock':
-                    // New mod stock
-                    break;
-                default:
-                    // Appelle la fonction du controleur pour afficher la page d'accueil de l'asso (qui est différent
-                    //selon les roles), : cette fonction va déterminer quel fonction de la vue appeller
-            }
-
+        $module = $_GET['module'];
+        switch ($module) {
+            case 'panier':
+                // new ModPanier
+                break;
+            case 'boutique':
+                // New mod boutique
+                break;
+            case 'stock':
+                // New mod stock
+                break;
+            case "asso" :
+                // Appelle le controleur de asso pour afficher la liste des assos
+                break;
+            default:
+                echo "module inconnue";
+                break;
         }
     }
 }
