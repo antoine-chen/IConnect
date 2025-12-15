d46a43ba0d66a92bff6e8130c88f45272bb90766
Antoine-Chen <antoinecoc2408@gmail.com>
2025-12-15 22:10:31 +0100

    correction de l'affichage catastrophique de la vue, vue de l'affichage lisible + style

--- modules/mod_asso/vue_asso.php
+++ modules/mod_asso/vue_asso.php
@@ -2,8 +2,7 @@
 include_once "vue_generique.php";
 
 class VueAsso extends VueGenerique {
-    public function __construct()
-    {
+    public function __construct(){
         parent::__construct();
     }
 
@@ -11,18 +10,19 @@ class VueAsso extends VueGenerique {
         return $this->getAffichage();
     }
 
-    public function afficherListeAsso($associations)
-    {
+    public function afficherListeAsso($associations){
+        echo '<section class="d-flex align-items-center flex-column gap-4">';
         foreach ($associations as $elementAsso) {
-            ?>
-            <div>
-                <a href="index.php?module=asso&action=choisiAsso&id=<?php echo htmlspecialchars($elementAsso['id']); ?>">
-                    <img src="<?php echo htmlspecialchars($elementAsso['image']); ?>" width="50">
-                </a>
-                <p><?php echo htmlspecialchars($elementAsso['nom']); ?></p>
-            </div>
-            <?php
+            echo '
+                <div>
+                    <a href="index.php?module=asso&action=choisiAsso&id='.$elementAsso['id'].'">
+                      <img src="'. $elementAsso['image'] .'" style="width: 600px; height: 300px;" alt="">
+                      <h6>'. htmlspecialchars($elementAsso['nom']). '</h6>
+                   </a>
+                </div>
+            '; 
         }
+        echo '</section>';
     }
 
 }
