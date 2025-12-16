72b7e967dac2d43a87da2b6ada86eeacd290e107
Antoine-Chen <antoinecoc2408@gmail.com>
2025-12-16 18:04:29 +0100

    responsive liste asso

--- modules/mod_asso/vue_asso.php
+++ modules/mod_asso/vue_asso.php
@@ -11,18 +11,25 @@ class VueAsso extends VueGenerique {
     }
 
     public function afficherListeAsso($associations){
-        echo '<section class="d-flex align-items-center flex-column gap-4">';
+        echo '
+        <div class="d-flex justify-content-center align-items-center">
+            <div class="d-flex flex-column gap-4">
+
+        ';
         foreach ($associations as $elementAsso) {
             echo '
                 <div>
                     <a href="index.php?module=asso&action=choisiAsso&id='.$elementAsso['id'].'">
-                      <img src="'. $elementAsso['image'] .'" style="width: 600px; height: 300px;" alt="">
+                      <img src="'. $elementAsso['image'] .'" class="img-association" alt="">
                       <h6>'. htmlspecialchars($elementAsso['nom']). '</h6>
                    </a>
                 </div>
             '; 
         }
-        echo '</section>';
+        echo '
+            </div>
+        </div>    
+        ';
     }
 
 }
