ddbac815adf80fa831b061d70ab942e0ed6d537d
Antoine-Chen <antoinecoc2408@gmail.com>
2025-12-18 20:24:39 +0100

    commit pour switch

--- Composants/comp_navbar/vue_navbar.php
+++ Composants/comp_navbar/vue_navbar.php
@@ -47,7 +47,7 @@ class VueNavbar extends VueGenerique {
                     <img src="modules/mod_asso/logos/logo.png" class="logo" alt="">
                 </div>
                 <div class="col-8 d-flex justify-content-center gap-4">
-                    navbar-item du barman
+                    <a href="index.php?module=commande&action=commandes">Commandes</a>
                 </div>
                 <div class="col">
                     <a href="index.php?actionComposant=deconnexion" class="btn btn-primary">Se déconnecter</a>
