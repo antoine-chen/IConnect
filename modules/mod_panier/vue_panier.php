85b0676f8ba01915bac91586a188ca66b51429b8
Antoine-Chen <antoinecoc2408@gmail.com>
2025-12-20 20:40:37 +0100

    ajout des htmlspecialchars

--- modules/mod_panier/vue_panier.php
+++ modules/mod_panier/vue_panier.php
@@ -15,14 +15,14 @@ class VuePanier extends VueGenerique{
                     <a href="index.php?module=panier&action=panier&id='. $produit['id'] .'">
                         <div class="d-flex justify-content-between align-items-center border rounded m-4">
                             <div class="d-flex align-items-center">
-                                <img src="'. $produit['image'] .'" alt="">
+                                <img src="'. htmlspecialchars($produit['image']) .'" alt="">
                                 <div>
-                                    <p>'.$produit['nom'].'</p>
-                                    <p>Qté : '.$produit['quantite'] .'</p>
+                                    <p>'. htmlspecialchars($produit['nom']).'</p>
+                                    <p>Qté : '. htmlspecialchars($produit['quantite']) .'</p>
                                 </div>
                             </div>
                             <div>
-                                <p>'.$produit['prix'] * $produit['quantite'].'€</p>
+                                <p>'. htmlspecialchars($produit['prix'] * $produit['quantite']).'€</p>
                             </div>
                         </div>
                     </a>
@@ -30,7 +30,7 @@ class VuePanier extends VueGenerique{
         }
         echo '
             <div class="d-flex justify-content-between align-items-center mx-5">
-                <p>Total : '. $addition.' €</p>
+                <p>Total : '. $addition .' €</p>
                 <input type="submit" value="Valider" class="btn btn-primary m-4">
             </div>
             </form>
