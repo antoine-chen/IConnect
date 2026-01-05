2557f54d232872b65fda47dd55b9542ee75a7a6d
tngomarie <tn.ngo.marie8@gmail.com>
2026-01-05 15:02:36 +0100

    finalisation de l'affichage des commande et details

--- modules/mod_commande/modele_commande.php
+++ modules/mod_commande/modele_commande.php
@@ -4,13 +4,23 @@ class ModeleCommande extends connexion {
 
     //fait une requette pour toute les commandes d'une association
     public function toutesLesCommandes(){
-        $req = self::$bdd->prepare("SELECT * from commande where idAssociation= ?");
+        $req = self::$bdd->prepare("SELECT * from commande 
+                                    where idAssociation= ?");
+        $req->execute([$_SESSION['asso']]);
+        return $req->fetchAll();
+    }
+    //fait une requette pour toute les commandes d'une association
+    public function toutesLesCommandesNonLivrée(){
+        $req = self::$bdd->prepare("SELECT * from commande 
+                                    where idAssociation= ? and status not like livrée");
         $req->execute([$_SESSION['asso']]);
         return $req->fetchAll();
     }
     //recupere toutes les ligne de commandes d'une commande
     public function derouléCommande($idCommande){
-        $req = self::$bdd->prepare("SELECT idCommande,nom,quantite from ligneCommande inner join produit on idProduit=id where idCommande =? ");
+        $req = self::$bdd->prepare("SELECT idCommande,nom,quantite 
+                                    from ligneCommande inner join produit on idProduit=id 
+                                    where idCommande =? ");
         $req->execute([$idCommande]);
         return $req->fetchAll();
     }
