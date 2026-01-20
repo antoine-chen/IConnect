d740f99bcde92cf79307c5e1cbb052ecd693c9e6
Jc <jeanchristophe.lay0@gmail.com>
2026-01-20 10:34:12 +0100

    Modification du code que l'ajout d'un fournisseur ajoute également l'asso, et que l'affichage des fournisseurs affiche que ceux de l'asso

--- modules/mod_fournisseur/modele_fournisseur.php
+++ modules/mod_fournisseur/modele_fournisseur.php
@@ -3,14 +3,14 @@
 class ModeleFournisseur extends Connexion{
 
     public function getListeFournisseur(){
-        $fournisseur = self::$bdd->prepare('SELECT * FROM fournisseur');
-        $fournisseur->execute();
+        $fournisseur = self::$bdd->prepare('SELECT * FROM fournisseur where idAssociation = (?)');
+        $fournisseur->execute([$_SESSION['asso']]);
         return $fournisseur->fetchAll();
     }
 
     public function insertFournisseur($nom, $email, $ville, $telephone){
-        $insertFournisseur = self::$bdd->prepare('INSERT INTO fournisseur(nom, email, ville, tel) VALUES (?, ?, ?, ?)');
-        $insertFournisseur->execute([$nom, $email, $ville, $telephone]);
+        $insertFournisseur = self::$bdd->prepare('INSERT INTO fournisseur(nom, email, ville, tel,idAssociation) VALUES (?, ?, ?, ?,?)');
+        $insertFournisseur->execute([$nom, $email, $ville, $telephone,$_SESSION['asso']]);
     }
 
     public function deleteFournisseur($idFournisseur){
